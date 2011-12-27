aec4e8b964e3293acb2d084e983e3051c150cf61
Orne Brocaar <info@brocaar.com>
2011-12-27 11:58:19 +0100

    Test added for language template-tag.

--- i18nurls/tests/templatetags_tests.py
+++ i18nurls/tests/templatetags_tests.py
@@ -1,3 +1,4 @@
+from django.template.loader import render_to_string
 from django.utils import translation
 from i18nurls.templatetags.i18nurls import override
 from i18nurls.tests.base import BaseTestCase
@@ -13,3 +14,13 @@ class OverrideTest(BaseTestCase):
             self.assertEqual('nl', translation.get_language())
 
         self.assertEqual('en', translation.get_language())
+
+
+class LanguageTest(BaseTestCase):
+
+    def test_language(self):
+        translation.activate('en')
+        self.assertEqual(
+            '/en/news/ /nl/nieuws/',
+            render_to_string('i18nurls/template_tag_test.html', {})
+        )
